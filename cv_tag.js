ccac2848f4405bd64ba78510e45b1bb1fc50fe81
Tetsuya <tetsuyas1@gmail.com>
2018-07-18 21:09:26 +0900

    add cv_tag

---
+++ cv_tag.js
@@ -0,0 +1,43 @@
+function loadScript(url, callback) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+
+  if ( script.readyState ) {
+    script.onreadystatechange = function() {
+      if ( script.readyState === 'loaded' || script.readyState === 'complete' ) {
+        script.onreadystatechange = null;
+        callback();
+      };
+    };
+  } else {
+    script.onload = function() {
+      callback();
+    };
+  };
+
+  document.getElementsByTagName('head')[0].appendChild(script);
+};
+
+loadScript("https://code.jquery.com/jquery-3.3.1.min.js", function(){
+  function set_cookie(){
+    var Nowymdhms　=　new Date();
+    var NowHour = Nowymdhms.getHours();
+    var NowMin = Nowymdhms.getMinutes();
+    var NowSec = Nowymdhms.getSeconds();
+    if(NowHour < 10){
+      NowHour = "0"+NowHour;
+    }
+    if(NowMin < 10){
+      NowMin = "0"+NowMin;
+    }
+    var h = NowHour+":"+NowMin+":"+NowSec;
+
+    console.log("==Media Sample Page== : " + h);
+    document.cookie = "medeia_com_cv1=" + h;
+    console.log(document.cookie);
+    $('#cookie_text').text(document.cookie);
+  }
+  setInterval(set_cookie, 2000)
+
+});
