8da3b59bcad464a53845c9352c640e1660d667b9
Tetsuya <tetsuyas1@gmail.com>
2018-07-18 21:57:29 +0900

    fix

--- cv_tag.js
+++ cv_tag.js
@@ -14,29 +14,47 @@ function loadScript(url, callback) {
     script.onload = function() {
       callback();
     };
-  };
+  }
 
   document.getElementsByTagName('head')[0].appendChild(script);
-};
+}
+
+function gethour(){
+  var Nowymdhms　=　new Date();
+  var NowHour = Nowymdhms.getHours();
+  var NowMin = Nowymdhms.getMinutes();
+  var NowSec = Nowymdhms.getSeconds();
+  if(NowHour < 10){
+    NowHour = "0"+NowHour;
+  }
+  if(NowMin < 10){
+    NowMin = "0"+NowMin;
+  }
+  var h = NowHour+":"+NowMin+":"+NowSec;
+  return h;
+}
 
 loadScript("https://code.jquery.com/jquery-3.3.1.min.js", function(){
   function set_cookie(){
-    var Nowymdhms　=　new Date();
-    var NowHour = Nowymdhms.getHours();
-    var NowMin = Nowymdhms.getMinutes();
-    var NowSec = Nowymdhms.getSeconds();
-    if(NowHour < 10){
-      NowHour = "0"+NowHour;
-    }
-    if(NowMin < 10){
-      NowMin = "0"+NowMin;
-    }
-    var h = NowHour+":"+NowMin+":"+NowSec;
 
     console.log("==Media Sample Page== : " + h);
     document.cookie = "cv_tag_js=" + h;
     console.log(document.cookie);
     $('#cv').text(document.cookie);
+
+    document.cookie.split(';').forEach(function(c) {
+      key = c.trim().split('=')[0];
+      if (key === "medeia_com_cv"){
+        alert("広告効果を測定しました。" + c.trim().split('=')[1] +"にクリックした広告です。");
+      }else{
+        alert("広告効果は測定できません。");
+      }
+    });
+
+
+
+
+
   }
   setInterval(set_cookie, 2000)
 });
